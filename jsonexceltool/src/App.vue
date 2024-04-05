ffa02c8ce6499f8d6522ff6bd39b4bccaabbfa25
joselopez117 <74109741+joselopez117@users.noreply.github.com>
2024-04-05 17:52:56 -0400

    feat: initial commit

---
+++ jsonexceltool/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import JsonExcelTool from "./components/JsonExcelTool.vue";
+</script>
+
+<template>
+  <JsonExcelTool />
+</template>
+
+<style scoped></style>
---
+++ jsonexceltool/src/components/JsonExcelTool.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <h1>JsonExcelTool</h1>
+  <p>Hello world! This tool was made to convert jsons to excel</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
