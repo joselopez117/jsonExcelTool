931d40af38946ab1dcf0cbcb8a94e1f120e40b7b
joselopez117 <74109741+joselopez117@users.noreply.github.com>
2024-05-01 18:37:14 -0400

    feat: adding buttons

--- jsonexceltool/src/components/JsonExcelTool.vue
+++ jsonexceltool/src/components/JsonExcelTool.vue
@@ -1,8 +1,12 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+</script>
 
 <template>
   <h1>JsonExcelTool</h1>
   <p>Hello world! This tool was made to convert jsons to excel</p>
+  <button>Convert Json to Excel</button>
+  <button>Convert Excel to Json</button>
 </template>
 
 <style scoped>
